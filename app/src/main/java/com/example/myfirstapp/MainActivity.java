f3935d03e0a4d10f0a86b4250befc3a7d0da2646
Seungchan Back <31721097+Backseungchan@users.noreply.github.com>
2020-12-16 19:22:53 +0900

    first update
    first update

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar ab = getSupportActionBar();
+        ab.hide();
+    }
+
+    public void onLoginButtonClicked(View v){
+        Intent intent = new Intent(getApplicationContext(), BoardActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+
+}
